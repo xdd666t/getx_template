00d6bfe001e6f541e59a9fcf85ea687bb52260ce
小呆呆666 <xdd666t@gmail.com>
2021-09-16 23:35:11 +0800

    greatly optimize setting layout

--- src/main/kotlin/setting/SettingsComponent.kt
+++ src/main/kotlin/setting/SettingsComponent.kt
@@ -1,20 +1,17 @@
 package setting
 
-import com.intellij.refactoring.suggested.main
 import com.intellij.ui.IdeBorderFactory
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
-import com.intellij.util.ui.FormBuilder
 import net.miginfocom.layout.CC
 import net.miginfocom.layout.LC
 import net.miginfocom.swing.MigLayout
-import java.awt.AWTEventMulticaster.add
 import java.awt.BorderLayout
-import java.awt.Container
 import java.awt.Dimension
-import java.awt.Toolkit
-import javax.naming.CompositeName
-import javax.swing.*
+import java.awt.GridLayout
+import javax.swing.BorderFactory
+import javax.swing.BoxLayout
+import javax.swing.JLabel
+import javax.swing.JPanel
 
 
 class SettingsComponent {
@@ -22,75 +19,68 @@ class SettingsComponent {
     var mainPanel = JPanel()
 
     @JvmField
-    var page = PageSetting()
+    var page = Setting()
 
     @JvmField
-    var component = ComponentSetting()
+    var component = Setting()
 
     @JvmField
-    var custom = CustomSetting()
+    var custom = Setting()
 
 
     init {
-
-        val temp = JPanel(migLayout()).apply {
-            border = IdeBorderFactory.createTitledBorder("Component")
-
-            add(JLabel("settings.label.translation.engine"))
-
-            val body = JPanel(migLayout()).apply {
-                add(JBTextField(20), wrap())
-            }
-
-            add(body, fillX())
-        }
+        val pageLayout = dealLayout(title = "Page", data = page)
+        val componentLayout = dealLayout(title = "Component", data = component)
+        val customLayout = dealLayout(title = "Custom", data = custom)
 
         mainPanel.layout = migLayoutVertical()
-        mainPanel.add(temp, fillX())
+        mainPanel.add(pageLayout, fillX())
+        mainPanel.add(componentLayout, fillX())
+        mainPanel.add(customLayout, fillX())
         mainPanel.add(JPanel(), fillY())
 
-//        mainPanel = FormBuilder.createFormBuilder()
-//            .addLabeledComponent(JPanel().apply {
-//                border = IdeBorderFactory.createTitledBorder("Component")
-//            }, JPanel())
-//            .addLabeledComponent(JBLabel("LogicName: "), page.logic)
-//            .addLabeledComponent(JBLabel("StateName: "), page.state)
-//            .addLabeledComponent(JBLabel("ViewName: "), page.view)
-//            .addLabeledComponent(JBLabel("ViewFileName: "), page.viewFile)
-//            .addLabeledComponent(JBLabel(""), dealMargin(JPanel()))
-//            .addLabeledComponent(JBLabel("Template-Component"), JPanel())
-//            .addLabeledComponent(JBLabel("LogicName: "), component.logic)
-//            .addLabeledComponent(JBLabel("StateName: "), component.state)
-//            .addLabeledComponent(JBLabel("ViewName: "), component.view)
-//            .addLabeledComponent(JBLabel("ViewFileName: "), component.viewFile)
-//            .addLabeledComponent(JBLabel(""), dealMargin(JPanel()))
-//            .addLabeledComponent(JBLabel("Template-Custom"), JPanel())
-//            .addLabeledComponent(JBLabel("LogicName: "), custom.logic)
-//            .addLabeledComponent(JBLabel("StateName: "), custom.state)
-//            .addLabeledComponent(JBLabel("ViewName: "), custom.view)
-//            .addLabeledComponent(JBLabel("ViewFileName: "), custom.viewFile)
-//            .addComponentFillVertically(JPanel(), 0)
-//            .panel
+    }
+
+    private fun dealLayout(title: String, data: Setting): JPanel {
+        return JPanel().apply {
+            layout = migLayout()
+            border = IdeBorderFactory.createTitledBorder(title)
+
+            val body = JPanel().apply {
+                layout = GridLayout(2, 2)
+
+                add(dealItem(title = "ViewName", jbTextField = data.view))
+
+                add(dealItem(title = "LogicName", jbTextField = data.logic))
+
+                add(dealItem(title = "ViewFile", jbTextField = data.viewFile))
+
+                add(dealItem(title = "StateName", jbTextField = data.state))
+            }
+            add(body, fillX())
+        }
+    }
+
+    private fun dealItem(title: String, jbTextField: JBTextField): JPanel {
+        return JPanel().apply {
+            layout = migLayout()
+            border = BorderFactory.createEmptyBorder(0,0,15,70)
+
+            add(JPanel().apply {
+                layout = BorderLayout()
+                preferredSize = Dimension(80, 30)
+                //left:WEST   right:EAST  top:NORTH  bottom:SOUTH  center:CENTER
+                add(JLabel(title), BorderLayout.WEST)
+            })
+
+            add(jbTextField, fillX())
+        }
     }
 
 
-    private fun dealMargin(jPanel: JPanel): JPanel {
-        jPanel.border = BorderFactory.createEmptyBorder(0, 0, 10, 0)
-        return jPanel
-    }
-
-//    private fun titledPanel(title: String, body: JPanel.() -> Unit): JComponent {
-//        val innerPanel = JPanel()
-//        innerPanel.body()
-//        return JPanel(migLayout()).apply {
-//            border = IdeBorderFactory.createTitledBorder(title)
-//            add(innerPanel)
-//            add(JPanel(), fillX())
-//        }
-//    }
 }
 
-class PageSetting {
+class Setting {
     var logic = JBTextField()
 
     var state = JBTextField()
@@ -100,31 +90,11 @@ class PageSetting {
     var viewFile = JBTextField()
 }
 
-class ComponentSetting {
-    var logic = JBTextField()
-
-    var state = JBTextField()
-
-    var view = JBTextField()
-
-    var viewFile = JBTextField()
-}
-
-class CustomSetting {
-    var logic = JBTextField()
-
-    var state = JBTextField()
-
-    var view = JBTextField()
-
-    var viewFile = JBTextField()
-}
 
 fun fillX(): CC = CC().growX().pushX()
 fun fillY(): CC = CC().growY().pushY()
-fun wrap(): CC = CC().wrap()
 fun migLayout() =
     MigLayout(LC().fill().gridGap("0!", "0!").insets("0"))
+
 fun migLayoutVertical() =
     MigLayout(LC().flowY().fill().gridGap("0!", "0!").insets("0"))
-fun fill(): CC = CC().grow().push()
